f5800405cc68b0019f0357603596a45a5706891d
Pradeep Rathor <pradeep.rathor@cyfuture.com>
2023-10-26 12:04:24 +0530

    woring on contract generation

--- src/com/jci/controller/DataMapper.java
+++ src/com/jci/controller/DataMapper.java
@@ -1,5 +1,11 @@
 package com.jci.controller;
 
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+@Service
 public class DataMapper {
 
+	//public Context setContext();
+	
 }
