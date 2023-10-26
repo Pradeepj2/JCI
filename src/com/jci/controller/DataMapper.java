b103eefb271d8099f72b1f241a8465066a7b1df6
Pradeep Rathor <pradeep.rathor@cyfuture.com>
2023-10-26 14:43:56 +0530

    working after remove pdf generation part

--- src/com/jci/controller/DataMapper.java
+++ src/com/jci/controller/DataMapper.java
@@ -1,11 +1,11 @@
-package com.jci.controller;
-
-import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-
-@Service
-public class DataMapper {
-
-	//public Context setContext();
-	
-}
+//package com.jci.controller;
+//
+//import org.springframework.stereotype.Service;
+//import org.thymeleaf.context.Context;
+//
+//@Service
+//public class DataMapper {
+//
+//	//public Context setContext();
+//	
+//}
