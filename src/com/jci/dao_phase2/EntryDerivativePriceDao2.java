9ff1b1310d64cc5cc914c41a95930708dea7c244
Pradeep Rathor <pradeep.rathor@cyfuture.com>
2023-11-08 17:31:01 +0530

    before dewali

--- src/com/jci/dao_phase2/EntryDerivativePriceDao2.java
+++ src/com/jci/dao_phase2/EntryDerivativePriceDao2.java
@@ -8,6 +8,8 @@ public interface EntryDerivativePriceDao2 {
 	public void create(EntryDerivativePrice edp);
 	public void update(EntryDerivativePrice edp);
 	public EntryDerivativePrice edit(int id);
+
+	/* comment for check */
 	public void delete(int id);
 	public EntryDerivativePrice findById(int id);
 	public List <EntryDerivativePrice> getAllEDP();
